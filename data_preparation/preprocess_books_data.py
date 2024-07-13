64c3031144bcaf02f8bca9b3311d57cc1e95fb04
danielepolotow <danielepolotow@gmail.com>
2024-07-13 19:21:33 -0300

    add preprocess book data

---
+++ data_preparation/preprocess_books_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def preprocess_books_data(file_path):
+    books_data = pd.read_csv(file_path)
+
+    books_data['authors'] = books_data['authors'].apply(
+        lambda x: eval(x) if isinstance(x, str) and x.startswith('[') else [x])
+
+    books_data_exploded = books_data.explode('authors')
+
+    books_data_exploded.to_csv('preprocessed_books_data.csv', index=False)
+
+
+preprocess_books_data('../data/books_data.csv')
+
